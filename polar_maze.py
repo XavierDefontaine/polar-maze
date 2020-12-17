9b175b4a0a0229058ae5ed9fcbf60ecfae4021dd
John Baxter <john.g.baxter@googlemail.com>
2020-12-17 13:41:02 +0000

    Add first passing test for new initialise_player() function. Function changes element[0,0] to 1.

--- polar_maze.py
+++ polar_maze.py
@@ -4,3 +4,7 @@ def make_grid(row, column = None):
   if column == None:
     column = row
   return np.zeros((row, column), dtype = int)
+
+def initialise_player(grid):
+  grid[0,0] = 1
+  return grid
--- polar_maze_test.py
+++ polar_maze_test.py
@@ -1,6 +1,9 @@
 import unittest
+from unittest import mock
+from unittest.mock import patch
 import numpy as np
 from polar_maze import make_grid
+from polar_maze import initialise_player
 
 class MakeGridTest(unittest.TestCase):
   def test_check_grid_has_a_single_zero_row(self):
@@ -12,6 +15,30 @@ class MakeGridTest(unittest.TestCase):
     expected_result = np.array([[0,0],[0,0]])
     actual_result = make_grid(2,2)
     np.testing.assert_array_equal(expected_result, actual_result)
+
+class InitialisePlayerTest(unittest.TestCase):
+  def test_player_starts_in_top_left_and_is_represented_by_1(self):
+    grid = np.array([
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+    ])
+    expected_result = np.array([
+      [1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ])
+    actual_result = initialise_player(grid)
+    np.testing.assert_array_equal(expected_result, actual_result)
+
+
+    
+
+    
 
 if __name__ == '__main__':
   unittest.main(verbosity = 2)
